f6352b4404722ee773d93720d85ee8d87e8e0f48
Akolade <emmakolade@gmail.com>
2022-08-08 16:04:17 -0700

    printer registry

---
+++ _printers_registry.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _get_printer - function that checks for a gestered priter for a given specifier
+ * @specifier: the specifier to get a printer for
+ *
+ * Return: the printer found
+ * if no valid printer exists, a default NULL-based printer is returned
+ */
+printer _get_printer(const char *specifier)
+{
+	int i;
+
+	static printer printers[] = {
+		{"c", _print_char},
+		{"s", _print_str},
+		{"d", _print_decimal},
+		{"i", _print_int},
+		{NULL, NULL}
+	};
+
+	for (i = 0;printers[i].specifier != NULL; i++)
+	{
+		if (*specifier == *(printers[i].specifier))
+			break;
+	}
+	return (printer[i]);
+}
