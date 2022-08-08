e764e6048cbe62342e03a5da7782f0a9708b03e7
somto-ez <adaeze.somy@gmail.com>
2022-08-08 15:45:36 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,42 +1,43 @@
-#include <stdarg.h>
 #include "main.h"
-#include <stdio.h>
 
 int _printf(const char *format, ...)
-{	int count = 0;
-	int i;
-	
+{
 	va_list printed;
+
+	printer printer;
+
+	int i = 0;
+	int count = 0;
 
 	va_start(printed, format);
 
-	
-        /* i is the array of format*/
-
-	for (i = 0; format[i] != '\0'; )
+	while (format[i])
 	{
-		if (format[i] != '%')
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			count += _putchar(format[i]);
-			i++;
+		_putchar(format[i];
+		count++;
 		}
-		else if (format[i] == '%' && format[i + 1] != ' ')
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					count += _putchar(va_arg(printed, int));
-				break;
 
-				case 's':
-					count += print_string(va_arg(printed, char*));
-					break;
-				default:
-				break;
-			}
+		if (!format[i])
+			break;
+
+			printer = _get_printer(&format[i + 1]);
+			if (printer.specifier != NULL)
+			{
+			count += printer.run(printed);
 			i += 2;
-		}
+			continue;
+			}
+			_putchar(format[i]);
+			count++;
+
+			if (format[i + 1] == '%')
+			i += 2;
+
+			else
+				i++;
 	}
 	va_end(printed);
-return (count);
+	return (count);
 }
