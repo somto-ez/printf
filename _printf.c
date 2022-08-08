b415dee5b8d856557da3c03a7014cc2f606abdc9
Akolade <emmakolade@gmail.com>
2022-08-08 09:25:31 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	va_list printed;
+
+	va_start(printed, format);
+
+	int count = 0, i; /* i is the array of format*/
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] != ' ')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					count += _putchar(va_arg(printed, int);
+				break;
+			}
+		}
+	}
+}
