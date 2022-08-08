095625dbc65ebd6cf55161a92ce4bc2f05c7cd59
somto-ez <adaeze.somy@gmail.com>
2022-08-08 16:07:41 -0700

    main

--- main.h
+++ main.h
@@ -1,8 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct_pointer - structure for printing to stdout
+ * @specifier: specifier recognized by printer
+ * @run: the function to run when printer is invoked
+ */
+
+typedef struct printer
+{
+	char *specifier;
+	int (*run)(va_list);
+}printer;
+
+printer _get_printer(const char *specifier);
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_string(char *);
+int _print_str(va_list);
+int _print_decimal(va_list);
+int _print_int(va_list);
 
 #endif
