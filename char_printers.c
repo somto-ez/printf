f04e193d82cfc8bb1bc2f8a60c09e5db0e5ce7a8
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-09 21:17:14 +0100

    Update char_printers.c

--- char_printers.c
+++ char_printers.c
@@ -3,13 +3,13 @@
 /**
  * _print_char - prints a character
  * @printed: argument list
- * Return 1
+ * Return: 1
  */
 
 int _print_char(va_list printed)
 {
 	_putchar(va_arg(printed, int));
-	return 1;
+	return (1);
 }
 
 /**
@@ -22,6 +22,7 @@ int _print_str(va_list printed)
 {
 	int add;
 	char *str = va_arg(printed, char *);
+	
 	if (str == NULL)
 		str = "(null)";
 	for (add = 0; str[add]; add++)
